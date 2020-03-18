7f4a8af7dce3af4b5fb86c8ef0ac3501f51cbfa2
schmicky <george.serban@ctbuc.ro>
2020-03-18 15:46:36 +0200

    ok

--- src/routes/brauletul.svelte
+++ src/routes/brauletul.svelte
@@ -10,8 +10,7 @@
     <div class="text-center">
       <iframe
         title="Brauletul"
-		width="100%"
-        
+        width="100%"
         height="400px"
         src="https://www.youtube.com/embed/a9kPH4kH4DM?start=59"
         frameborder="0"
@@ -23,11 +22,46 @@
 
   <div class="col-12">
     <p class="mt-4">
-      Ansamblul cu o istorie de peste 50 de ani continuă să reprezinte cultura
-      și tradițiile românești la diferite ocazii festive, promovându-le prin
-      intermediul unor spectacole cu joc și voie bună. Ne mândrim cu un
-      repertoriu ce include suite din toate zonele folclorice ale României, de
-      la Dobrogea la Ardeal, Moldova, Banat și multe altele.
+      Când vorbim despre o formaţie ori ansamblu de dans popular aici în
+      Dobrogea, numele care ne vine pe buze aproape automat este „Brâuleţul”.
+      Acest nume a intrat aşa de mult în mentalul colectiv încât ni se pare
+      firesc să auzim de el că evoluează pe o scenă sau pe alta. Şi dacă la
+      vremea la care aceste ansambluri populare s-au înfiinţat (respectiv anii
+      ’50) „Brâuleţul” era o marcă neînregistrată, spre deosebire de alte
+      ansambluri care erau mărci înregistrate precum „Ciprian Porumbescu” pentru
+      Suceava ori „Periniţa” pentru Bucureşti, la ora actuală ansamblul este
+      marcă înregistrată.
+    </p>
+    <p>
+      Când s-a constituit ansamblul trupa de dansatori amatori s-a reunit sub
+      bagheta maestrului coregraf Ion Buruc, prin anul 1953. Aceasta a
+      constituit în fapt prima formă organizată de dansuri populare din
+      Dobrogea. S-a numit „Brâuleţul”. A funcţionat ca formaţie de amatori până
+      în 15 octombrie 1962.
+    </p>
+    <img
+    src="foto/Biserica 04.jpg"
+    class="img-fluid"
+    style="width: 100%; height: auto; border-radius: 7px;"
+    alt="Responsive image" />
+    <p class="mt-4">
+      În momentul de față ansamblul a fost preluat de către Fundația Demnitatea
+      Pontică din Constanța. În anul 2012 ansamblul a susținut un spectacolul
+      aniversar, spectacol care a reprezentat în același timp și spectacolul de
+      revenire a ansamblului pe scena dansului popular.
+    </p>
+    <div class="mx-auto mt-4">
+      <img
+        class="rounded mx-auto d-block"
+        src="foto/Brauletul 50 de ani.jpg"
+        style="max-width:100%; height: auto;"
+        alt="Responsive image" />
+    </div>
+    <p class="mt-4">
+      Ansamblul numără în momentul de față un număr de 20 de dansatori, dintre
+      care unii sunt din vechiul ansamblu iar altă parte au fost cooptați în
+      ansamblu de-a lungul anilor, vârsta dansatorilor fiind cuprinsă între 16
+      și 53 de ani.
     </p>
   </div>
 
--- src/routes/toate-proiectele/_posts.js
+++ src/routes/toate-proiectele/_posts.js
@@ -11,6 +11,95 @@ const posts = [
   {
     beneficiar: "Brauletul",
     title:
+      "Spectacol Aniversar “Brâulețul - 50 de ani de la înființare, 1962 - 2012” – 12 Decembrie 2012, Casa de Cultură a Sindicatelor Constanța;",
+    slug: "brauletul-aniversare-50-ani",
+
+    html: `
+			<p>Spectacol Aniversar “Brâulețul - 50 de ani de la înființare, 1962 - 2012” – 12 Decembrie 2012, Casa de Cultură a Sindicatelor Constanța;</p>
+		`,
+    // foto: ["89480205_2906399932716037_620021019289059328_o.jpg"],
+    videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    // iframe: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2012-03-12",
+    linkExtern: "",
+    tip: "brauletul",
+    suma: ""
+  },
+
+  {
+    beneficiar: "Brauletul",
+    title:
+      "Turneu “Săptămâna românească” – Martie-Aprilie 2017 - Zona Bordeaux, Franța;",
+    slug: "brauletul-saptamana-romaneasca-bordeaux",
+
+    html: `
+			<p>Turneu “Săptămâna românească” – Martie-Aprilie 2017 - Zona Bordeaux, Franța;</p>
+		`,
+    // foto: ["89480205_2906399932716037_620021019289059328_o.jpg"],
+    videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    // iframe: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2017-03-01",
+    linkExtern: "",
+    tip: "brauletul",
+    suma: ""
+  },
+
+  {
+    beneficiar: "Brauletul",
+    title:
+      "Spectacol festiv “Ziua Internațională a Dansului” – 29 Aprilie 2017, Casa de Cultură a Sindicatelor Constanța;",
+    slug: "brauletul-zona-internationala-a-dansului",
+
+    html: `
+			<p>Spectacol festiv “Ziua Internațională a Dansului” – 29 Aprilie 2017, Casa de Cultură a Sindicatelor Constanța;</p>
+		`,
+    // foto: ["89480205_2906399932716037_620021019289059328_o.jpg"],
+    videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    // iframe: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2017-04-29",
+    linkExtern: "",
+    tip: "brauletul",
+    suma: ""
+  },
+
+  {
+    beneficiar: "Brauletul",
+    title:
+      "Spectacol caritabil “În pași de dans spre suflete minunate” – strângere de fonduri pentru copiii bolnavi de diabet, 31 Mai 2017, Casa de Cultură a Sindicatelor Constanța;",
+    slug: "brauletul-in-pasi-de-dans-spre-suflete-minunate",
+
+    html: `
+			<p>Spectacol caritabil “În pași de dans spre suflete minunate” – strângere de fonduri pentru copiii bolnavi de diabet, 31 Mai 2017, Casa de Cultură a Sindicatelor Constanța;</p>
+		`,
+    // foto: ["89480205_2906399932716037_620021019289059328_o.jpg"],
+    videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    // iframe: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2017-05-31",
+    linkExtern: "",
+    tip: "brauletul",
+    suma: ""
+  },
+
+  {
+    beneficiar: "Brauletul",
+    title: "“Balkan Folk Fest 2015” – 14 Mai 2015, Velico-Târnovo, Bulgaria;",
+    slug: "brauletul-balkan-folk-fest-2015",
+
+    html: `
+			<p>“Balkan Folk Fest 2015” – 14 Mai 2015, Velico-Târnovo, Bulgaria;</p>
+		`,
+    // foto: ["89480205_2906399932716037_620021019289059328_o.jpg"],
+    videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    // iframe: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2015-05-14",
+    linkExtern: "",
+    tip: "brauletul",
+    suma: ""
+  },
+
+  {
+    beneficiar: "Brauletul",
+    title:
       'Ansamblul Folcloric "Brâulețul" a fost prezent la Restaurant Dorna in cadrul spectacolului Lăutari din Chișinău și Maestrul Nicolae Botgros.',
     slug: "brauletul-la-restaurant-dorna",
 
@@ -44,6 +133,21 @@ const posts = [
     },
     tip: "caritate",
     suma: "40,000 EUR"
+  },
+  {
+    beneficiar: "Familia Dascalu",
+    title: "Sprijin pentru familia Dascalu - Proiect recurent",
+    slug: "familia-dascalu",
+    html: `
+			<p>Fundaţia “Demnitatea Pontică” ii sprijina material pe cei 6 copii ai familiei Dascălu din Constanţa, ai căror părinţi au murit în floarea vârstei unul după altul.</p>
+      <p>Cel mai mare copil are 16 ani, cel mai mic are 12 ani. Cristian s-a născut în 2001, gemenii Andrei şi Tudor - în 2003, gemenii Amelia şi Luca - în 2005, iar Marta în 2006. Perechile de gemeni, chiar, s-au născut în aceeaşi zi: 27 ianuarie. Copiii au intrat sub tutela bunicii Maria Uţă, care a primit custodia lor prin hotărâre judecătorească definitivă.</p>
+      `,
+    foto: ["fam-dascalu.png"],
+    //videoFb: ["https://www.facebook.com/Brauletul/videos/842509842438400/"],
+    //iframe:`<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FBrauletul%2Fposts%2F2906400119382685&width=500" width="500" height="670" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+    data: "2016-01-01",
+    tip: "caritate"
+    // suma: "40,000 EUR"
   }
 ];
 
